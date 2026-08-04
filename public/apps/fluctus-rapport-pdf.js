8bb2461d7b0e16d63f6249339eca7d382966aed0
JohanMMK <oekene@gmail.com>
2026-08-04 21:00:11 +0200

    Add files via upload

--- public/apps/fluctus-rapport-pdf.js
+++ public/apps/fluctus-rapport-pdf.js
@@ -87,7 +87,7 @@
   }
 
   // v1.1 (Johan-review 03-08): scherpe bestandsnaam = <type>_<projectID> (bv. Contract_FLX-…, SolarActive_FLX-…).
-  function fname(){ var jc=(function(){ try{ return /(^|[?&])stijl=jacops(&|$)/.test(location.search||''); }catch(e){ return false; } })();
+  function fname(){ var jc=(function(){ try{ if(window.__STIJL_JACOPS) return true; }catch(e){} try{ return /(^|[?&])stijl=jacops(&|$)/.test(location.search||''); }catch(e){ return false; } })();
     return (M.type+(M.pid?'_'+M.pid:'')+(jc?'_jacops':'')).replace(/[^A-Za-z0-9._-]/g,'_')+'.pdf'; }
 
   async function upload(b64){
